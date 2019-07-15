4fda51a6769cf5c270ee86b0de647734ac970dfc
Allan Castro <AllanC@magenic.com>
2019-07-15 17:21:05 +0800

    added routes

--- flutter_masters/lib/main.dart
+++ flutter_masters/lib/main.dart
@@ -18,14 +18,24 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: MyHomePage(title: 'Ola flttr'), //commented this out because it will throw an error when we declare initial route
+      // Start the app with the "/" named route. In this case, the app starts
+      // on the FirstScreen widget.
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => MyHomePage(title: 'Ola flttr'),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/x': (context) => PageX(),
+        '/y': (context) => PageY(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget { //this is my first "route"
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -45,7 +55,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  bool pressed = false;
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -75,8 +85,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
+          /// Column is also layout widget. It takes a list of children and
+          /// arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
           //
           // Invoke "debug painting" (press "p" in the console, choose the
@@ -92,12 +102,47 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'F.A.B. click count:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+
+            FlatButton(
+              onPressed: () {
+                setState(() {
+                 pressed = !pressed; 
+                });
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PageX()),
+                    );
+              },
+              color: Colors.blueGrey,
+              child: Column (children: <Widget>[
+                Text( "simple push and pop nav",
+                style: pressed
+                ? TextStyle(color: Colors.white)
+                : TextStyle(color: Colors.lightBlueAccent),
+                )
+              ],)
+            ),
+          
+            FlatButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/y');
+              },
+              color: Colors.blueGrey,
+              child: Column (children: <Widget>[
+                Text( "Named route navigation",
+                style: pressed
+                ? TextStyle(color: Colors.white)
+                : TextStyle(color: Colors.lightBlueAccent),
+                )
+              ],)
+            )
+          
           ],
         ),
       ),
@@ -108,4 +153,46 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+
+class PageX extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("page X"),
+          ),
+          body: Center(
+            child: RaisedButton(
+              onPressed: () {
+                  Navigator.pop(context);
+                  //Navigator.pushNamed(context, '/');
+                },
+            child: Text('Go back!'),
+          ),
+        ),
+      );
+    }
+}
+
+class PageY extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("page Y"),
+          ),
+          body: Center(
+            child:
+              RaisedButton(
+                onPressed: () {
+                  //Navigator.pop(context);
+                  Navigator.pushNamed(context, '/');
+                },
+            child: Text('Go back to main'),
+          ),
+        ),
+      );
+    }
 }
